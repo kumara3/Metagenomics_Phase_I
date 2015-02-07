b59e460151c48bfa8bac550973a5f56431adf821
Ashwani Kumar <kumara3@miamioh.edu>
2015-02-06 23:48:14 -0500

    Update draw_bar_graph.py

--- draw_bar_graph.py
+++ draw_bar_graph.py
@@ -3,15 +3,14 @@ Created on Aug 20, 2014
 
 @author: kumara3
 
-Input is meta-velvet.contigs.fa and the script should output x,y coordinate for the bar graph
-Using x,y coordinate 
+Input : meta-velvet.contigs.fa 
+OUTPUT : x,y coordinate. x is DNA sequence length and y is the number of reads 
 '''
 
 from _bisect import bisect
 from sys import argv
 import os
 import csv
-
 
 script,pathname = argv
 fasta_file = os.path.abspath(pathname)
@@ -43,22 +42,11 @@ def readFA(fa_file):
     return readfasta
 
 return_readfasta = readFA(fasta_file)
-"""
-for each in return_readfasta:
-    print each,":",len(return_readfasta[each])
-"""
-
-#spec = [y for x,y in zip(['A','B','C','D','E','F','G','H','I','J','K','L','M'],['1000','2000','3000','4000','5000','6000','7000','8000','9000','10000','110000','12000','13000'])]
-#specs = ''.join(spec)
 
 def draw_bar(return_fa):
     siz=0
     dict_bar={}
     sizes = ([len(return_fa[key]) for key in return_fa])
-    #specs = [y for x,y in zip(['A','B','C','D','E','F','G','H','I','J','K','L','M'],['1000','2000','3000','4000','5000','6000','7000','8000','9000','10000','110000','12000','13000'])]
-    #spec = ''.join(specs)
-
-    #organize(siz,breakpoints=[1000,2000,3000,4000,9000,6000,7000,8000,9000,10000,11000,12000],specs='ABCDEFGHIJKLM'))
     list_bar = [organize(siz)for siz in sizes]
     mapping_dict = {x:y for x,y in zip(['A','B','C','D','E','F','G','H','I','J','K','L'],['1000','2000','3000','4000','5000','6000','7000','8000','9000','10000','11000','>=11000'])}
 
@@ -71,21 +59,12 @@ def draw_bar(return_fa):
     except:
         print "check the bisect algorithm parameter"
     
-    
-        #print i,":",dict_bar[i]
     with open(file_name,'wb') as fw:
             writer = csv.writer(fw)
             for key,value in dict_bar.items():
                 if key in mapping_dict.keys():
 
                     writer.writerow([mapping_dict[key],value])
-                #writer.writerows(str(dict_bar[i]))
-            #writer.writerows([dict_bar[key] for key in dict_bar])
-    
-    """
-    for key in dict_bar:
-        print key,":", dict_bar[key]
-    """     
     
 def organize(sizs,breakpoints=[1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000],specs='ABCDEFGHIJKL'):
     i = bisect(breakpoints,sizs)
